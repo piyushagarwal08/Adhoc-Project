145e489958005ba3b0285acf90364a5ebf9bc013
root <root@ip-172-31-46-172.ap-south-1.compute.internal>
2019-08-04 13:01:28 +0000

    Version1 Complete

--- Quiz/QuizMania/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class QuizmaniaConfig(AppConfig):
-    name = 'QuizMania'
--- Quiz/QuizMania/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-        path("",views.index),
-        path("महादेव?myvar=41",views.test,name="test"),
-        ]
-
